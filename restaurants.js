1e0f27cb7aeff95f71f9945244597f9c2ea13f01
cragon0608 <cragon0608@gmail.com>
2026-09-16 21:26:05 +0700

    Add files via upload

---
+++ restaurants.js
@@ -0,0 +1,64 @@
+// File: restaurants.js - Dữ liệu danh sách quán ăn
+const restaurants = [
+  {
+    id: "quan-1",
+    name: "Bún Riêu Tuấn Anh",
+    category: "Bún",
+    lat: 20.998638862273015,
+    lng: 105.85170226643129,
+    address: "35-37 Hồng Mai",
+    rating: "",
+    hours: "07:00–13:30 / 17:00–22:30",
+    menu: [
+      { name: "Bún riêu", price: "25K", img_url: "" },
+      { name: "Riêu bò", price: "35K", img_url: "" },
+      { name: "Riêu giò tai", price: "35K", img_url: "" },
+      { name: "Riêu chả cá", price: "35K", img_url: "" },
+      { name: "Riêu sườn sụn", price: "35K", img_url: "" },
+      { name: "Riêu trứng vịt lộn", price: "35K", img_url: "" },
+      { name: "Riêu bò giò", price: "40K", img_url: "" },
+      { name: "Riêu bò sườn sụn", price: "40K", img_url: "" },
+      { name: "Riêu bò trứng vịt lộn", price: "40K", img_url: "" },
+      { name: "Riêu đầy đủ", price: "50K", img_url: "" }
+    ]
+  },
+  {
+    id: "quan-2",
+    name: "Mát trà",
+    category: "Nước",
+    lat: 21.007207306646695,
+    lng: 105.84471256643153,
+    address: "31 Trần Đại Nghĩa",
+    rating: "",
+    hours: "???",
+    menu: [
+      { name: "Chưa rõ", price: "???", img_url: "" }
+    ]
+  },
+{
+    id: "quan-3",
+    name: "Mỳ cay seoul",
+    category: "Mỳ",
+    lat: 20.998179011500763, 
+    lng: 105.84600712410253,
+    address: "223 Trần Đại Nghĩa",
+    rating: "",
+    hours: "???",
+    menu: [
+      { name: "Chưa rõ", price: "???", img_url: "" }
+    ]
+  },
+{
+    id: "quan-4",
+    name: "Bánh Mì chảo cô Nụ",
+    category: "Bánh mì",
+    lat: 21.00021959686782,
+    lng:  105.84636456643136, 
+    address: "26 ngõ 183 Trần Đại Nghĩa",
+    rating: "",
+    hours: "09:00-21:00",
+    menu: [
+      { name: "", price: "", img_url: "images/banhmichaoconu.png" }
+    ]
+  },
+];
