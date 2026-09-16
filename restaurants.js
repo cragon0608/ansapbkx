e6301c05c63ae0e96e4f8dce616c41d6e633fc6f
cragon0608 <cragon0608@gmail.com>
2026-09-16 21:37:35 +0700

    restaurants.js

--- restaurants.js
+++ restaurants.js
@@ -61,4 +61,17 @@ const restaurants = [
       { name: "", price: "", img_url: "images/banhmichaoconu.png" }
     ]
   },
+  {
+    id: "quan-5",
+    name: "Big P - Khoai tây chiên",
+    category: "ăn vặt",
+    lat: 21.008641431149147,
+    lng: 105.8518073799246, 
+    address: "519 Đ. Trần Khát Chân, Bạch Mai, Hà Nội, Việt Nam",
+    rating: "",
+    hours: "11:00-23:00",
+    menu: [
+      { name: "", price: "", img_url: "images/bigp.png" }
+    ]
+  },
 ];
